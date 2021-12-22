bbab14ee0a27b185ccaa60b92e9382863d6c42dc
Aloïs DELAUNE <alois.delaune@hotmail.fr>
2021-12-22 12:37:45 +0100

    fixed and finished bootstrap install

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     Knp\DoctrineBehaviors\DoctrineBehaviorsBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
